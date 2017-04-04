276a5b7c586b7cc1412682043f30ebe98fe78d6a
Justin Rose <jinks.mysteryman@gmail.com>
2017-04-04 15:15:10 -0700

    GameSelectViewController adds cells and downloaded images. Looks horrible. Will fix once functionality of app is finished.

--- HW Assassin/View Controllers/GameSelectViewController.swift
+++ HW Assassin/View Controllers/GameSelectViewController.swift
@@ -10,10 +10,17 @@ import UIKit
 import CoreData
 import Alamofire
 
-class GameSelectViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class GameSelectTableViewCell: UITableViewCell{
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var statusLabel: UILabel!
+    @IBOutlet weak var numPlayersLabel: UILabel!
+    @IBOutlet weak var gameImageView: UIImageView!
+}
+
+class GameSelectViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, NSFetchedResultsControllerDelegate {
 
     @IBOutlet weak var tableView: UITableView!
-    
+    var fetchedResultsController: NSFetchedResultsController<Game>?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,7 +28,7 @@ class GameSelectViewController: UIViewController, UITableViewDataSource, UITable
         
         let headers = ["Content-Type": "application/json"]
         
-        Alamofire.request("http://hwassassin.hwtechcouncil.com/api/games/", method: .get, parameters: nil, encoding: JSONEncoding.default, headers: headers).responseJSON{ [unowned self] response in
+        Alamofire.request("http://hwassassin.hwtechcouncil.com/api/games/", method: .get, parameters: nil, encoding: JSONEncoding.default, headers: headers).responseJSON{ response in
             debugPrint(response)
             
             if let status = response.response?.statusCode {
@@ -35,11 +42,32 @@ class GameSelectViewController: UIViewController, UITableViewDataSource, UITable
             //to get JSON return value
             if let result = response.result.value {
                 let JSON = result as! NSArray
-                
                 print("Response JSON: \(JSON)")
+                
+                for g in JSON as! [[String: AnyObject]]{
+                    let game = Game(context: AppDelegate.viewContext)
+                    game.id = g["id"] as! Int64!
+                    game.name = g["name"] as? String
+                    game.status = g["status"] as? String
+                    game.pictureURL = g["game_picture"] as? String
+                }
+                
+                print("Created games")
             }
-            
         }
+        let request: NSFetchRequest<Game> = Game.fetchRequest()
+        request.sortDescriptors = [NSSortDescriptor(key: "id", ascending: false)]
+        fetchedResultsController = NSFetchedResultsController<Game>(fetchRequest: request, managedObjectContext: AppDelegate.viewContext, sectionNameKeyPath: nil, cacheName: "GameQueryCache")
+        
+        fetchedResultsController?.delegate = self
+        
+        do{
+            try fetchedResultsController?.performFetch()
+        }
+        catch let error{
+            print("Caught error \(error)")
+        }
+        tableView.reloadData()
     }
 
     override func didReceiveMemoryWarning() {
@@ -47,19 +75,107 @@ class GameSelectViewController: UIViewController, UITableViewDataSource, UITable
         // Dispose of any resources that can be recreated.
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        self.navigationItem.setHidesBackButton(true, animated: true)
+        super.viewDidAppear(animated)
+    }
     
     // MARK: - UITableViewDataSource
     
     func numberOfSections(in tableView: UITableView) -> Int {
+        //return fetchedResultsController?.sections?.count ?? 1
         return 1
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        if let sections = fetchedResultsController?.sections, sections.count>0 {
+            return sections[section].numberOfObjects
+        }
+        else{
+            return 0
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if let sections = fetchedResultsController?.sections, sections.count>0 {
+            return sections[section].name
+        }
+        else{
+            return nil
+        }
+    }
+    
+    func sectionIndexTitles(for tableView: UITableView) -> [String]? {
+        return fetchedResultsController?.sectionIndexTitles
+    }
+    
+    func tableView(_ tableView: UITableView, sectionForSectionIndexTitle title: String, at index: Int) -> Int {
+        return fetchedResultsController?.section(forSectionIndexTitle: title, at: index) ?? 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        let cell = tableView.dequeueReusableCell(withIdentifier: "game_select_cell", for: indexPath) as! GameSelectTableViewCell
+        
+        if let obj = fetchedResultsController?.object(at: indexPath){
+            print(obj)
+            cell.nameLabel.text = obj.name
+            
+            switch(obj.status!){
+            case "r":
+            cell.statusLabel.text = "Registration"
+            case "p":
+            cell.statusLabel.text = "In Progress"
+            case "c":
+            cell.statusLabel.text = "Complete"
+            default:
+            cell.statusLabel.text = "Error"
+            }
+            
+            //temporary
+            cell.numPlayersLabel.text = "50 players"
+            
+            Alamofire.request(obj.pictureURL!).responseData{ response in
+                debugPrint(response)
+                
+                if let data = response.result.value, let image = UIImage(data: data) {
+                    cell.gameImageView.image = image
+                }
+            }
+        }
+        
+        return cell
+    }
+    
+    // MARK: - NSFetchedResultsControllerDelegate
+    
+    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.beginUpdates()
+    }
+    
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange sectionInfo: NSFetchedResultsSectionInfo, atSectionIndex sectionIndex: Int, for type: NSFetchedResultsChangeType) {
+        switch type {
+        case .insert: tableView.insertSections([sectionIndex], with: .fade)
+        case .delete: tableView.deleteSections([sectionIndex], with: .fade)
+        default:break
+        }
+    }
+    
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
+        switch type{
+        case .insert:
+            tableView.insertRows(at: [newIndexPath!], with: .fade)
+        case .delete:
+            tableView.deleteRows(at: [indexPath!], with: .fade)
+        case .update:
+            tableView.reloadRows(at: [indexPath!], with: .fade)
+        case .move:
+            tableView.deleteRows(at: [indexPath!], with: .fade)
+            tableView.insertRows(at: [newIndexPath!], with: .fade)
+        }
+    }
+    
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.endUpdates()
     }
     
     
